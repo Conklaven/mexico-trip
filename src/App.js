83778390ff4a0c9dd8ffd749a02e33f5a4d4a556
Conklaven <94914434+Conklaven@users.noreply.github.com>
2025-01-08 12:58:24 -0600

    Image Updates

--- src/App.js
+++ src/App.js
@@ -60,12 +60,12 @@ const Itinerary = () => {
     { 
       date: 'April 14th', 
       activity: 'Drive from Memphis to St. Louis',
-      images: ['/images/day1.jpg', '/images/day1-2.jpg', '/images/day1-3.jpg']
+      images: ['https://www.brookings.edu/wp-content/uploads/2024/04/memphis-tennessee-skyline-downtown.jpg', 'https://www.billburmaster.com/rmsandw/tennessee/images/55us61nus647079.jpg', 'https://www.visittheusa.com/sites/default/files/styles/hero_l/public/images/hero_media_image/2024-11/a9ede22e-9522-4316-ac67-7589c60a76cc.png']
     },
     { 
       date: 'April 15th', 
       activity: 'Flight to Cancun - Private transport to JOIA Paraiso',
-      images: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ92MmQK9lhRWoh73xZVpk-x4ECUnrKs6VG4A&shttps://content.r9cdn.net/rimg/dimg/7d/53/6c3c1580-al-AA-55b57246.jpg', '/images/day2-2.jpg', '/images/day2-3.jpg']
+      images: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ92MmQK9lhRWoh73xZVpk-x4ECUnrKs6VG4A&shttps://content.r9cdn.net/rimg/dimg/7d/53/6c3c1580-al-AA-55b57246.jpg', 'https://www.fodors.com/wp-content/uploads/2023/07/0-HERO-istockphoto-1224372937.jpg', 'https://www.cancuncare.com/wp-content/uploads/2019/11/cancun-airport-transfers.jpg']
     },
     { 
       date: 'April 16th', 
@@ -80,27 +80,27 @@ const Itinerary = () => {
     { 
       date: 'April 18th', 
       activity: 'Relax at JOIA Paraiso',
-      images: ['https://images.trvl-media.com/lodging/3000000/2810000/2802500/2802472/4900729f.jpg', '/images/day5-2.jpg']
+      images: ['https://images.trvl-media.com/lodging/3000000/2810000/2802500/2802472/4900729f.jpg', 'https://images.trvl-media.com/lodging/3000000/2810000/2802500/2802472/34718a74.jpg']
     },
     { 
       date: 'April 19th', 
       activity: 'Relax at JOIA Paraiso',
-      images: ['/images/day6-1.jpg', '/images/day6-2.jpg']
+      images: ['https://images.trvl-media.com/lodging/3000000/2810000/2802500/2802472/b2ae5616.jpg', 'https://digital.ihg.com/is/image/ihg/independent-riviera-maya-9146268166-2x1']
     },
     { 
       date: 'April 20th', 
       activity: 'Relax at JOIA Paraiso',
-      images: ['/images/day7-1.jpg', '/images/day7-2.jpg']
+      images: ['https://images.trvl-media.com/lodging/3000000/2810000/2802500/2802472/a2517adb.jpg', 'https://images.trvl-media.com/lodging/3000000/2810000/2802500/2802472/14ecf498.jpg']
     },
     { 
       date: 'April 21st', 
       activity: 'Private transport to Intercontinental Cancun',
-      images: ['/images/day8-1.jpg', '/images/day8-2.jpg']
+      images: ['https://images.trvl-media.com/lodging/1000000/20000/13000/12937/c80fa3fa.jpg', 'https://images.trvl-media.com/lodging/1000000/20000/13000/12937/72ad7caf.jpg']
     },
     { 
       date: 'April 22nd', 
       activity: 'Flight home to St. Louis',
-      images: ['/images/day9-1.jpg', '/images/day9-2.jpg']
+      images: ['https://static1.simpleflyingimages.com/wordpress/wp-content/uploads/2022/08/American-Airlines-Boeing-737-MAX-8-N310RF-(3).jpg']
     }
   ];
 
